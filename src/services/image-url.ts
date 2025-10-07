b53ae69c931e49e691c799b2ebe87e7422627317
Hamed <hsalmanizadegan@gmail.com>
2025-10-07 23:30:26 +0330

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholder from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string, width: number = 600, height: number = 400) => {
-  if (!url) return null;
+  if (!url) return noImagePlaceholder;
   let target = "media/";
   const inedx = url.indexOf(target) + target.length;
   return url.slice(0, inedx) + "crop/" + width + "/" + height +"/" + url.slice(inedx); 
